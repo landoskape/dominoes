537f322b1df69022b79df111261765aa5c7bc534
landoskape <andrewtylerlandau@gmail.com>
2023-08-21 23:35:31 +0100

    Working on league manager

--- leagueManager.py
+++ leagueManager.py
@@ -12,30 +12,44 @@ import dominoesFunctions as df
 # then the league manager will update ELOs and 
 # I want to equip the agent manager with some methods for measuring the ELO of each agent 
 class leagueManager:
-    def __init__(self, highestDominoe, numPlayers, replace=False, device=None, **metaParameters):
+    def __init__(self, highestDominoe, numPlayers, shuffleAgents=True, replace=False, device=None):
         self.highestDominoe = highestDominoe
         self.numPlayers = numPlayers
         self.dominoes = df.listDominoes(highestDominoe)
         self.numDominoes = len(self.dominoes)
         self.replace = replace
+        self.shuffleAgents = shuffleAgents
         self.agents = []
         self.numAgents = 0
         self.device = device if device is not None else "cuda" if torchCuda.is_available() else "cpu"
         
     def addAgents(self, agents):
         # Wrapper for "addAgent" when there are multiple pre-instantiated agents to add
-        None
+        for idx, agent in enumerate(agents): 
+            # Check if all agents in list are valid first
+            assert self.checkAgent(agent), f"Agent #{idx} is not a dominoe agent"
+            assert isinstance(agent, da.dominoeAgent), f"Agent #{idx} must be an instantiated object of a dominoe agent"
+            assert self.checkParameters(agent), f"Agent #{idx} has the wrong game parameters (either numPlayers or highestDominoe)"
+        for agent in agents:
+            # Then once you know they are valid, add all of them (double assertions are worth it for expected behavior)
+            self.addAgent(agent)
         
     def addAgent(self, agent):
         # This method adds a single instantiated agent to the league
-        None
+        assert self.checkAgent(agent), "agent is not a dominoe agent"
+        assert isinstance(agent,da.dominoeAgent), "agent must be an instantiated object of a dominoe agent
+        assert self.checkParameters(agent), "agent has the wrong game parameters (either numPlayers or highestDominoe"
+        agent.device = self.device # update device of agent
+        self.agents.append(agent)
+        self.numAgents += 1
         
     def addAgentType(self, agentType, num2add=1):
         # This method adds a new agent to the league
         # "agentType" is a class that is a subtype of "dominoeAgent" (not an instantiated agent)
         # Can add multiple instantiations of this agent by changing the num2add parameter
         assert isinstance(num2add, int) and num2add>0, "num2add must be a positive integer"
-        assert self.checkAgent(agentType) and not(isinstance(agentType,da.dominoeAgent)), "agentType must be a class definition of a dominoeAgent"
+        assert self.checkAgent(agentType), "agentType is not a dominoe agent"
+        assert not(isinstance(agentType,da.dominoeAgent)), "agentType must be a class definition of a dominoeAgent, not an instantiated object"
         for _ in range(num2add):
             self.agents.append(agentType(self.numPlayers, self.highestDominoe, self.dominoes, self.numDominoes, device=self.device))
             self.numAgents += 1
@@ -43,76 +57,49 @@ class leagueManager:
     def checkAgent(self, agent):
         # Supporting function to make sure that "agent" is either a instance of a dominoe agent or a class definition
         return hasattr(agent, 'className') and agent.className=='dominoeAgent'
-        
-    def createTable(self, numPlayers):
-        # This method creates a game table using the agents in the league
-        None
 
-    def updateElo(self, gameTable, gameResults):
+    def checkParameters(self, agent):
+        # for instantiated agents, check that their metaparameters are consistent with the league parameters
+        return (self.highestDominoe in agent.highestDominoeRange) and (self.numPlayers in agent.numPlayerRange)
+        
+    def createGameTable(self, numPlayers=None):
+        # This method creates a game table using the agents in the league
+        numPlayers = numPlayers is numPlayers is not None else self.numPlayers
+        if self.replace:
+            leagueIndex = random.choices(range(self.numAgents), k=numPlayers)
+        else:
+            leagueIndex = random.sample(range(self.numAgents), k=numPlayers)
+        agentList = [self.agents[idx] for idx in agentIndex]
+        return gameTable(self.highestDominoe, agentList, self.shuffleAgents), leagueIndex
+
+    def updateElo(self, leagueIndex, gameResults):
         # This method updates the ELO ratings of the agents in the game based on the gameResults
         None
 
 
-assert (numPlayers is not None) or (agents is not None), "either numPlayers or agents need to be specified"
-        if (numPlayers is not None) and (agents is not None): 
-            assert numPlayers == len(agents), "the number of players specified does not equal the number of agents provided..."
-        if numPlayers is None: 
-            numPlayers = len(agents)
-            
-        self.numPlayers = numPlayers
+class gameTable:
+    def __init__(self, highestDominoe, agentList, shuffleAgents=False):
+        self.numPlayers = len(agentList)
         self.highestDominoe = highestDominoe
         self.dominoes = df.listDominoes(highestDominoe)
         self.numDominoes = len(self.numDominoes)
         self.shuffleAgents = shuffleAgents
-        self.device = device if device is not None else "cuda" if torchCuda.is_available() else "cpu"
         
         # create an index for managing shuffling of agents
         self.originalAgentIndex = [idx for idx in range(self.numPlayers)]
         
-        # create agents 
-        if agents is None: agents = [defaultAgent]*self.numPlayers # if agents is None, set them to the default agent
-        # Check that each agent is either None or a valid instance/class of a dominoeAgent
-        for idx, agent in enumerate(agents): 
-            if agent is not None: 
-                assert hasattr(agent, 'className') and agent.className=='dominoeAgent', f"Agent {idx} is of type: {type(agent)}. You must only provide dominoeAgents!"
-        agents = [agent if agent is not None else defaultAgent]
-        self.agents = [None]*self.numPlayers
-        for agentIdx,agent in enumerate(agents):
-            if isinstance(agent, da.dominoeAgent):
-                # if the agent is an instance of the dominoeAgent, then that means this agent is already instantiated
-                # we need to check that they have the right parameters (i.e. numPlayers and highestDominoe), then add them to the agent manager
-                assert (agent.numPlayers==numPlayers) and (agent.highestDominoe==highestDominoe), f"provided agent (agentIdx:{agentIdx}) did not have the correct number of players or dominoes"
-                self.agents[agentIdx] = agent
-                self.agents[agentIdx].agentIndex = agentIdx
-                self.agents[agentIdx].device = device
-            else:
-                self.agents[agentIdx] = agent(numPlayers, highestDominoe, self.dominoes, self.numDominoes, agentIdx, device=device)
+        for idx, agent in enumerate(agents):
+            assert isinstance(agent, da.dominoeAgent), f"Agent{idx} is not a dominoeAgent"
+            assert (self.numPlayers in agent.numPlayerRange) and (self.highestDominoe in agent.highestDominoeRange), f"Agent{idx} has wrong number of parameters"
+            agent.setAgentIndex(idx)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # ----------------
+    # -- functions for managing agents --
+    # ----------------
+    def getAgent(self, agentIndex):
+        assert agentIndex in self.originalAgentIndex, "requested agent index does not exist"
+        idxAgent = self.originalAgentIndex.index(agentIndex)
+        return self.agents[idxAgent]
 
 
 
