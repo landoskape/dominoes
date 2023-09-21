604243d712d55457d5a455ee640d8e59d1aac5d7
landoskape <andrewtylerlandau@gmail.com>
2023-09-21 13:24:19 +0100

    Added replay to trainValueAgent experiment

--- experiments/trainValueAgent.py
+++ experiments/trainValueAgent.py
@@ -22,19 +22,6 @@ from dominoes import gameplay as dg
 from dominoes import agents as da
 from dominoes import functions as df
 
-parser = argparse.ArgumentParser(description='Run dominoes experiment.')
-parser.add_argument('-n','--num-players', type=int, default=4, help='the number of agents in the game of dominoes')
-parser.add_argument('-hd','--highest-dominoe', type=int, default=9, help='the highest dominoe in the board')
-parser.add_argument('-s','--shuffle-agents', type=bool, default=True, help='whether to shuffle the order of the agents each hand')
-parser.add_argument('-tg','--train-games',type=int, default=2000, help='the number of training games')
-parser.add_argument('-tr','--train-rounds',type=int, default=None, help='the number of training rounds')
-parser.add_argument('-op','--opponent',type=str, default='dominoeAgent', help='which opponent to play the basic value agent against for training and testing')
-parser.add_argument('-va','--value-agent',type=str, default='basicValueAgent', help='which value agent to use')
-parser.add_argument('--justplot',default=False,action='store_true', help='if used, will only plot the saved results (results have to already have been run and saved)')
-parser.add_argument('--nosave',default=False,action='store_true')
-
-args = parser.parse_args()
-
 valueAgents = {
     'basicValueAgent':da.basicValueAgent,
     'lineValueAgent':da.lineValueAgent,
@@ -49,9 +36,6 @@ opponents = {
     'persistentLineAgent':da.persistentLineAgent
 }
 
-assert args.value_agent in valueAgents.keys(), f"requested value agent ({args.value_agent}) is not in the list of possibilities!"
-assert args.opponent in opponents.keys(), f"requested opponent ({args.opponent}) is not in the list of possible opponents!"
-
 # can edit this for each machine it's being used on
 savePath = Path('.') / 'experiments' / 'savedNetworks'
 resPath = Path('.') / 'experiments' / 'savedResults'
@@ -61,9 +45,30 @@ figsPath = Path(mainPath) / 'docs' / 'media'
 device = "cuda" if torchCuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Run dominoes experiment.')
+    parser.add_argument('-n','--num-players', type=int, default=4, help='the number of agents in the game of dominoes')
+    parser.add_argument('-hd','--highest-dominoe', type=int, default=9, help='the highest dominoe in the board')
+    parser.add_argument('-s','--shuffle-agents', type=bool, default=True, help='whether to shuffle the order of the agents each hand')
+    parser.add_argument('-tg','--train-games',type=int, default=2000, help='the number of training games')
+    parser.add_argument('-tr','--train-rounds',type=int, default=None, help='the number of training rounds')
+    parser.add_argument('-op','--opponent',type=str, default='dominoeAgent', help='which opponent to play the basic value agent against for training and testing')
+    parser.add_argument('-va','--value-agent',type=str, default='basicValueAgent', help='which value agent to use')
+    parser.add_argument('--noreplay',default=False,action='store_true', help='if used, will turn off replay in the value agents')
+    parser.add_argument('--justplot',default=False,action='store_true', help='if used, will only plot the saved results (results have to already have been run and saved)')
+    parser.add_argument('--nosave',default=False,action='store_true')
+
+    args = parser.parse_args()
+    
+    assert args.value_agent in valueAgents.keys(), f"requested value agent ({args.value_agent}) is not in the list of possibilities!"
+    assert args.opponent in opponents.keys(), f"requested opponent ({args.opponent}) is not in the list of possible opponents!"
+     
+    return args
+
 # method for returning the name of the saved network parameters (different save for each possible opponent)
 def getFileName():
-    return f"trainValueAgent_{args.value_agent}_against_{args.opponent}"
+    replayString = '' if args.noreplay else 'withReplay_'
+    return f"trainValueAgent_{args.value_agent}_"+replayString+f"against_{args.opponent}"
 
 # method for training agent
 def trainValueAgent(numPlayers, highestDominoe, shuffleAgents, trainGames, trainRounds):
@@ -71,6 +76,7 @@ def trainValueAgent(numPlayers, highestDominoe, shuffleAgents, trainGames, train
     agents=(valueAgents[args.value_agent], None, None, None)
     game = dg.dominoeGame(highestDominoe, numPlayers=numPlayers, shuffleAgents=shuffleAgents, agents=agents, defaultAgent=opponents[args.opponent], device=device)
     game.getAgent(0).setLearning(True)
+    if args.noreplay: game.getAgent(0).setReplay(False)
     
     # run training rounds
     trainWinnerCount = np.zeros(numPlayers)
@@ -133,6 +139,8 @@ def plotResults(results):
 
 # Main script
 if __name__=='__main__': 
+    args = parseArgs()
+    
     # Sorry for my improper style
     numPlayers = args.num_players
     highestDominoe = args.highest_dominoe
