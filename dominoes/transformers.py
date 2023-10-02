faf533b6d174f5d207648d37c0e1139fa7a0e708
landoskape <andrewtylerlandau@gmail.com>
2023-10-02 17:41:43 +0100

    All networks can mask uneven sequences now

--- dominoes/transformers.py
+++ dominoes/transformers.py
@@ -118,7 +118,9 @@ class SelfAttention(nn.Module):
             # mask query key products to -inf that are not used
             dotMask = torch.bmm(mask.unsqueeze(2), mask.unsqueeze(1))
             dotMask = dotMask.unsqueeze(1).expand(batch, self.heads, tokens, tokens).reshape(batch*self.heads, tokens, tokens)
-            dot.masked_fill_(dotMask==0, float('-inf')) # assign -inf for any value that isn't used
+            dot += (dotMask + 1e-45).log()
+            # this produces nans for any row that is fully masked 
+            #dot.masked_fill_(dotMask==0, float('-inf')) # assign -inf for any value that isn't used
         
         # and take softmax to get self-attention probabilities
         dot = F.softmax(dot, dim=2)
@@ -229,7 +231,9 @@ class ContextualAttention(nn.Module):
             # mask query key products to -inf that are not used
             dotMask = torch.bmm(mask.unsqueeze(2), mask_plus_contextMask.unsqueeze(1))
             dotMask = dotMask.unsqueeze(1).expand(batch, self.heads, itokens, tokens).reshape(batch*self.heads, itokens, tokens)
-            dot.masked_fill_(dotMask==0, float('-inf')) # assign -inf for any value that isn't used
+            dot += (dotMask + 1e-45).log()
+            # produces nans for any rows that are fully masked
+            #dot.masked_fill_(dotMask==0, float('-inf')) # assign -inf for any value that isn't used
         
         # and take softmax to get self-attention probabilities
         dot = F.softmax(dot, dim=2)
@@ -249,9 +253,8 @@ class PointerAttention(nn.Module):
     PointerAttention Module (as specified in the original paper)
 
     log_softmax is preferable if the probabilities of each token are not 
-    needed because it's more efficient. However, if token embeddings are
-    combined via their probabilities, then softmax is required, so log_softmax
-    should be set to `False`.
+    needed. However, if token embeddings are combined via their probabilities,
+    then softmax is required, so log_softmax should be set to `False`.
     """
     def __init__(self, emb, log_softmax=False):
         super().__init__()
@@ -261,14 +264,16 @@ class PointerAttention(nn.Module):
         self.W2 = nn.Linear(emb, emb, bias=False)
         self.vt = nn.Linear(emb, 1, bias=False)
 
-    def forward(self, encoded, decoder_state):
+    def forward(self, encoded, decoder_state, mask=None):
         # first transform encoded representations and decoder states 
         transformEncoded = self.W1(encoded)
-        transformDecoder = self.W2(decoder_state).unsqueeze(1) # unsqueeze for broadcasting on token dimension
+        transformDecoded = self.W2(decoder_state).unsqueeze(1) # unsqueeze for broadcasting on token dimension
 
         # then combine them and project to a new space
-        u = self.vt(torch.tanh(transformEncoded + transformDecoder)).squeeze(-1)
-
+        u = self.vt(torch.tanh(transformEncoded + transformDecoded)).squeeze(-1)
+        if mask is not None:
+            u.masked_fill_(mask==0, float('-inf')) # only use valid tokens
+            
         if self.log_softmax:
             # convert to log scores
             return torch.nn.functional.log_softmax(u, dim=-1)
@@ -370,9 +375,9 @@ class PointerNetwork(nn.Module):
         self.greedy = greedy
 
         self.embedding = nn.Linear(in_features=input_dim, out_features=self.embedding_dim, bias=self.bias)
-        self.encodingLayers = [TransformerLayer(self.embedding_dim, heads=self.heads, expansion=self.expansion, contextual=False, kqnorm=self.kqnorm) for _ in range(self.encoding_layers)]
-        self.encoder = nn.Sequential(*self.encodingLayers)
-
+        self.encodingLayers = nn.ModuleList([TransformerLayer(self.embedding_dim, heads=self.heads, expansion=self.expansion, contextual=False, kqnorm=self.kqnorm) for _ in range(self.encoding_layers)])
+        self.encoding = self.forwardEncoder # since there are masks, it's easier to write a custom forward than wrangle the transformer layers to work with nn.Sequential
+        
         if self.decoder_method == 'gru':
             # if using GRU, then make a GRU cell for the decoder
             self.decoder = nn.GRUCell(input_size=self.embedding_dim, hidden_size=self.embedding_dim, bias=self.bias)
@@ -382,26 +387,48 @@ class PointerNetwork(nn.Module):
             self.decoder = TransformerLayer(self.embedding_dim, heads=self.heads, expansion=self.expansion, contextual=True, kqnorm=self.kqnorm)
         
         else: 
-            raise ValueError("an unexpected bug occurred and self.decoder_method was not set correctly"
+            raise ValueError("an unexpected bug occurred and decoder_method was not set correctly"
                              f"it should be either 'gru' or 'attention', but it is {self.decoder_method}")
 
         # output of the network uses a pointer attention layer as described in the original paper
         self.pointer = PointerAttention(self.embedding_dim, log_softmax=self.greedy)
+
+    def forwardEncoder(self, x, mask=None):
+        """
+        instead of using nn.Sequential just call each layer in sequence
+        this solves the problem of passing in the optional mask to the transformer layer 
+        (it would have to be passed as an output by the transformer layer and I'd rather keep the transformer layer code clean)
+        """
+        for elayer in self.encodingLayers:
+            x = elayer(x, mask=mask)
+        return x
         
     def forward(self, x, mask=None): 
+        """
+        forward method for pointer network with possible masked input
+        
+        x should be an input tensor with shape (batchSize, maxTokens, input_dim)
+        mask should be a 1/0 input tensor with shape (batchSize, maxTokens) where a 1 indicates a valid token and 0 indicates padded data
+        checks on the mask only care about the shape, so make sure your mask is as described!!!
+        """
         assert x.ndim == 3, "x must have shape (batch, tokens, input_dim)"
         batch, tokens, inp_dim = x.size()
         assert inp_dim == self.input_dim, "input dim of x doesn't match network"
         if mask is not None: 
-            assert mask.ndim == 3, "mask must have shape (batch, tokens)"
+            assert mask.ndim == 2, "mask must have shape (batch, tokens)"
             assert mask.size(0)==batch and mask.size(1)==tokens, "mask must have same batch size and max tokens as x"
         else:
             mask = torch.ones((batch, tokens), dtype=x.dtype).to(get_device(x))
         
         # Encoding stage
         embeddedRepresentation = self.embedding(x) # embed each token to right dimensionality
-        encodedRepresentation = self.encoder(embeddedRepresentation) # perform N-layer self-attention on inputs
-        decoder_context = encodedRepresentation.mean(dim=1) # average encoded representations to get the context
+        encodedRepresentation = self.encoding(embeddedRepresentation, mask=mask) # perform N-layer self-attention on inputs
+        
+        # Get the masked mean of any encoded tokens
+        numerator = torch.sum(encodedRepresentation*mask.unsqueeze(2), dim=1)
+        denominator = torch.sum(mask, dim=1, keepdim=True)
+        decoder_context = numerator / denominator
+        #decoder_context = encodedRepresentation.mean(dim=1) # average encoded representations to get the context
         decoder_input = torch.zeros((batch, self.embedding_dim)).to(get_device(x)) # initialize decoder_input as zeros
 
         # Decoding stage
@@ -413,16 +440,17 @@ class PointerNetwork(nn.Module):
                 decoder_context = self.decoder(decoder_input, decoder_context)
             elif self.decoder_method == 'attention':
                 contextInputs = torch.cat((decoder_input.unsqueeze(1), encodedRepresentation), dim=1)
-                decoder_context = self.decoder((decoder_context.unsqueeze(1), contextInputs)).squeeze(1)
+                contextMask = torch.cat((torch.ones((batch,1), dtype=mask.dtype).to(get_device(mask)), mask), dim=1)
+                decoder_context = self.decoder((decoder_context.unsqueeze(1), contextInputs), contextMask=contextMask).squeeze(1)
             else:
-                raise ValueError("self.decoder_method not recognized")
+                raise ValueError("decoder_method not recognized")
                 
             # use pointer attention to evaluate scores of each possible input given the context
-            score = self.pointer(encodedRepresentation, decoder_context)
+            score = self.pointer(encodedRepresentation, decoder_context, mask=mask)
             
             # standard loss function (nll_loss) requires log-probabilities
             log_score = score if self.greedy else torch.log(score)
-            
+
             # choose token for this sample
             choice = torch.argmax(log_score, dim=1, keepdim=True)
 
--- experiments/pointerDemonstration.py
+++ experiments/pointerDemonstration.py
@@ -52,7 +52,7 @@ def handleArguments():
     
     return args
 
-def dominoeBatch(batchSize, seqLength, listDominoes, dominoeValue, highestDominoe):
+def dominoeUnevenBatch(batchSize, minSeq, maxSeq, listDominoes, dominoeValue, highestDominoe, ignoreIndex=-1):
     """
     retrieve a batch of dominoes and their target order given the value of each dominoe
 
@@ -68,6 +68,13 @@ def dominoeBatch(batchSize, seqLength, listDominoes, dominoeValue, highestDomino
     value. Note that many dominoes share the same value, but since the dominoe
     list is always the same, equal value dominoes will always be sorted in the
     same way. 
+    
+    Each element can have a different sequence length, they will be padded 
+    with zeros to whatever the longest sequence is. The ignoreIndex is used to
+    determine what to label targets for any padded elements (i.e. any place 
+    where no prediction is needed). The nll_loss function then accepts this as
+    an input to ignore. This is part of the reason why pointer networks are
+    awesome... the input and output can vary in size!!!
 
     The simple representation is a two-hot vector where the first 
     <highestDominoe+1> elements represent the first value of the dominoe, and
@@ -81,29 +88,99 @@ def dominoeBatch(batchSize, seqLength, listDominoes, dominoeValue, highestDomino
     (1 | 0): [0, 1, 0, 0, 1, 0, 0, 0]
     (2 | 1): [0, 0, 1, 0, 0, 1, 0, 0]
 
-    For simplification, every element in the batch has the same sequence 
-    length, but once I have a mask working, I can vary sequence length within
-    the batch. And that's why pointer networks are awesome. 
     """
     numDominoes = len(listDominoes)
     input_dim = 2*(highestDominoe+1)
-
-    # select list of seqLength dominoes
-    selection = np.stack([np.random.choice(numDominoes, seqLength, replace=False).tolist() for _ in range(batchSize)])
-    value = dominoeValue[selection]
-    sortIdx = [sorted(range(len(val)), key=lambda i : -val[i]) for val in value] # sort dominoes by value in decreasing order
-    target = torch.stack([torch.LongTensor(idx) for idx in sortIdx])
     
-    firstValue = torch.tensor(listDominoes[selection,0], dtype=torch.int64).unsqueeze(2)
-    secondValue = torch.tensor(listDominoes[selection,1], dtype=torch.int64).unsqueeze(2) + highestDominoe+1
+    # choose how long each sequence in the batch will be
+    seqLength = np.random.randint(minSeq, maxSeq+1, batchSize)
+    maxSeqLength = max(seqLength) # max sequence length for padding
 
-    input = torch.zeros((batchSize, seqLength, input_dim), dtype=torch.float)
-    input.scatter_(2, firstValue, torch.ones_like(firstValue, dtype=torch.float))
-    input.scatter_(2, secondValue, torch.ones_like(secondValue, dtype=torch.float))
+    # choose dominoes from the batch, and get their value (in points)
+    selection = [np.random.choice(numDominoes, sl, replace=False).tolist() for sl in seqLength]
+    value = [dominoeValue[sel] for sel in selection]
+    
+    # index of first and second value in two-hot representation
+    pad = [[0]*(maxSeqLength-sl) for sl in seqLength]
+    firstValue = np.stack([listDominoes[sel,0].tolist()+p for p, sel in zip(pad, selection)])
+    secondValue = np.stack([(listDominoes[sel,1]+highestDominoe+1).tolist()+p for p, sel in zip(pad, selection)])
+    firstValue = torch.tensor(firstValue, dtype=torch.int64).unsqueeze(2)
+    secondValue = torch.tensor(secondValue, dtype=torch.int64).unsqueeze(2)
 
-    return input, target
+    # create mask (used for scattering and also as an output)
+    mask = 1.*(torch.arange(maxSeqLength).view(1,-1).expand(batchSize, -1) < torch.tensor(seqLength).view(-1,1))
+
+    # scatter data into two-hot vectors, except where sequence length is exceed where the mask is 0
+    input = torch.zeros((batchSize, maxSeqLength, input_dim), dtype=torch.float)
+    input.scatter_(2, firstValue, mask.float().unsqueeze(2))
+    input.scatter_(2, secondValue, mask.float().unsqueeze(2))
+    
+    # sort and pad each list of dominoes by value
+    def sortPad(val, padTo, ignoreIndex=-1):
+        s = sorted(range(len(val)), key=lambda i: -val[i])
+        p = [ignoreIndex]*(padTo-len(val))
+        return s+p
+
+    # create a padded sort index, then turn into a torch tensor as the target vector
+    sortIdx = [sortPad(val, maxSeqLength, ignoreIndex) for val in value] # pad with ignore index so nll_loss ignores them
+    target = torch.stack([torch.LongTensor(idx) for idx in sortIdx])
+
+    return input, target, mask
+    
+# def dominoeBatch(batchSize, seqLength, listDominoes, dominoeValue, highestDominoe):
+#     """
+#     retrieve a batch of dominoes and their target order given the value of each dominoe
+
+#     dominoes are paired values (combinations with replacement) of integers 
+#     from 0 to <highestDominoe>. The total value of each dominoe is the sum of
+#     the two integers associated with that dominoe. For example, the dominoe
+#     (7|3) has value 10. 
+
+#     Each element in the batch contains an input and target. The input is 
+#     composed of a sequence of dominoes in a random order, transformed into a
+#     simple representation (explained below). The target is a list of the order
+#     of dominoes by the one with the highest value to the one with the lowest
+#     value. Note that many dominoes share the same value, but since the dominoe
+#     list is always the same, equal value dominoes will always be sorted in the
+#     same way. 
+
+#     The simple representation is a two-hot vector where the first 
+#     <highestDominoe+1> elements represent the first value of the dominoe, and
+#     the second <highestDominoe+1> elements represent the second value of the 
+#     dominoe. Here are some examples for highest dominoe = 3:
+
+#     (0 | 0): [1, 0, 0, 0, 1, 0, 0, 0]
+#     (0 | 1): [1, 0, 0, 0, 0, 1, 0, 0]
+#     (0 | 2): [1, 0, 0, 0, 0, 0, 1, 0]
+#     (0 | 3): [1, 0, 0, 0, 0, 0, 0, 1]
+#     (1 | 0): [0, 1, 0, 0, 1, 0, 0, 0]
+#     (2 | 1): [0, 0, 1, 0, 0, 1, 0, 0]
+
+#     For simplification, every element in the batch has the same sequence 
+#     length, but once I have a mask working, I can vary sequence length within
+#     the batch. And that's why pointer networks are awesome. 
+#     """
+#     numDominoes = len(listDominoes)
+#     input_dim = 2*(highestDominoe+1)
+
+#     # select list of seqLength dominoes
+#     selection = np.stack([np.random.choice(numDominoes, seqLength, replace=False).tolist() for _ in range(batchSize)])
+#     value = dominoeValue[selection]
+#     sortIdx = [sorted(range(len(val)), key=lambda i : -val[i]) for val in value] # sort dominoes by value in decreasing order
+#     target = torch.stack([torch.LongTensor(idx) for idx in sortIdx])
+    
+#     firstValue = torch.tensor(listDominoes[selection,0], dtype=torch.int64).unsqueeze(2)
+#     secondValue = torch.tensor(listDominoes[selection,1], dtype=torch.int64).unsqueeze(2) + highestDominoe+1
+
+#     input = torch.zeros((batchSize, seqLength, input_dim), dtype=torch.float)
+#     input.scatter_(2, firstValue, torch.ones_like(firstValue, dtype=torch.float))
+#     input.scatter_(2, secondValue, torch.ones_like(secondValue, dtype=torch.float))
+
+#     return input, target
 
 def trainTestModel():
+    ignoreIndex = -1
+    
     # get values from the argument parser
     highestDominoe = args.highest_dominoe
     listDominoes = df.listDominoes(highestDominoe)
@@ -131,17 +208,20 @@ def trainTestModel():
     
     # Train network
     print("Training network...")
-    trainSeqLength = torch.zeros(trainEpochs)
+    # trainSeqLength = torch.zeros(trainEpochs)
     trainLoss = torch.zeros(trainEpochs)
-    trainPositionError = [None]*trainEpochs
+    # trainPositionError = [None]*trainEpochs
     for epoch in tqdm(range(trainEpochs)):
         
         # right now elements in a batch have to have the same sequence length 
-        cSeqLength = np.random.randint(minSeqLength, maxSeqLength+1)
+        #cSeqLength = np.random.randint(minSeqLength, maxSeqLength+1)
 
         # create a data batch and put it on the right device
-        input, target = dominoeBatch(batchSize, cSeqLength, listDominoes, dominoeValue, highestDominoe)
-        input, target = input.to(device), target.to(device)
+        #input, target = dominoeBatch(batchSize, cSeqLength, listDominoes, dominoeValue, highestDominoe)
+        #input, target = input.to(device), target.to(device)
+
+        input, target, mask = dominoeUnevenBatch(batchSize, minSeqLength, maxSeqLength, listDominoes, dominoeValue, highestDominoe, ignoreIndex=ignoreIndex)
+        input, target, mask = input.to(device), target.to(device), mask.to(device)
 
         # zero gradients, get output of network
         optimizer.zero_grad()
@@ -149,63 +229,63 @@ def trainTestModel():
 
         # measure loss with negative log-likelihood
         unrolled = log_scores.view(-1, log_scores.size(-1))
-        loss = torch.nn.functional.nll_loss(unrolled, target.view(-1))
+        loss = torch.nn.functional.nll_loss(unrolled, target.view(-1), ignore_index=ignoreIndex)
         assert not np.isnan(loss.item()), "model diverged :("
 
         # update network
         loss.backward()
         optimizer.step()
 
-        # the loss spikes sometimes, and I think it might be due to the autoregressive component of pointer networks.
-        # i.e., if the network gets the first element wrong, it'll get the rest of the elements right given the one it chose... but this will inflate the training loss
-        # anyway, this metric is a way to measure how bad the prediction is as a function of position in sequence to try to identify that issue
-        t = torch.gather(unrolled, dim=1, index=target.view(-1).unsqueeze(-1)).cpu().detach().view(batchSize, cSeqLength)
-        m = torch.max(unrolled, dim=1)[0].cpu().detach().view(batchSize, cSeqLength)
-        trainPositionError[epoch] = torch.mean(m-t,dim=0)
+        # # the loss spikes sometimes, and I think it might be due to the autoregressive component of pointer networks.
+        # # i.e., if the network gets the first element wrong, it'll get the rest of the elements right given the one it chose... but this will inflate the training loss
+        # # anyway, this metric is a way to measure how bad the prediction is as a function of position in sequence to try to identify that issue
+        # t = torch.gather(unrolled, dim=1, index=target.view(-1).unsqueeze(-1)).cpu().detach().view(batchSize, cSeqLength)
+        # m = torch.max(unrolled, dim=1)[0].cpu().detach().view(batchSize, cSeqLength)
+        # trainPositionError[epoch] = torch.mean(m-t,dim=0)
 
         # save training data
         trainLoss[epoch] = loss.item()
-        trainSeqLength[epoch] = cSeqLength
+        # trainSeqLength[epoch] = cSeqLength
 
     # Test network - same thing as in testing but without updates to model
     with torch.no_grad():
         print("Testing network...")
         pnet.eval()
         
-        testSeqLength = torch.zeros(testEpochs)
+        # testSeqLength = torch.zeros(testEpochs)
         testLoss = torch.zeros(testEpochs)
-        testPositionError = [None]*testEpochs
+        # testPositionError = [None]*testEpochs
         for epoch in tqdm(range(testEpochs)):
             # input, target = getBatch(batchSize, seqLength, input_dim)
-            cSeqLength = np.random.randint(minSeqLength, maxSeqLength+1)
-            input, target = dominoeBatch(batchSize, cSeqLength, listDominoes, dominoeValue, highestDominoe)
-            input, target = input.to(device), target.to(device)
-        
+            # cSeqLength = np.random.randint(minSeqLength, maxSeqLength+1)
+            
+            # input, target = dominoeBatch(batchSize, cSeqLength, listDominoes, dominoeValue, highestDominoe)
+            # input, target = input.to(device), target.to(device)
+
+            input, target, mask = dominoeUnevenBatch(batchSize, minSeqLength, maxSeqLength, listDominoes, dominoeValue, highestDominoe, ignoreIndex=ignoreIndex)
+            input, target, mask = input.to(device), target.to(device), mask.to(device)
+
             log_scores, choices = pnet(input)
             
             unrolled = log_scores.view(-1, log_scores.size(-1))
-            loss = torch.nn.functional.nll_loss(unrolled, target.view(-1))
+            loss = torch.nn.functional.nll_loss(unrolled, target.view(-1), ignore_index=ignoreIndex)
             
-            t = torch.gather(unrolled, dim=1, index=target.view(-1).unsqueeze(-1)).cpu().detach().view(batchSize, cSeqLength)
-            m = torch.max(unrolled, dim=1)[0].cpu().detach().view(batchSize, cSeqLength)
-            testPositionError[epoch] = torch.mean(m-t,dim=0)
+            # t = torch.gather(unrolled, dim=1, index=target.view(-1).unsqueeze(-1)).cpu().detach().view(batchSize, cSeqLength)
+            # m = torch.max(unrolled, dim=1)[0].cpu().detach().view(batchSize, cSeqLength)
+            # testPositionError[epoch] = torch.mean(m-t,dim=0)
             testLoss[epoch] = loss.item()
-            testSeqLength[epoch] = cSeqLength
+            # testSeqLength[epoch] = cSeqLength
 
     results = {
         'trainLoss': trainLoss,
-        'trainSeqLength': trainSeqLength, 
-        'trainPositionError': trainPositionError,
         'testLoss': testLoss,
-        'testSeqLength': testSeqLength, 
-        'testPositionError': testPositionError
     }
 
     return results
 
 
 def plotResults(results, args):
-    fig, ax = plt.subplots(1,3,figsize=(12,4))
+    fig, ax = plt.subplots(1,2,figsize=(8,4))
     ax[0].plot(range(args.train_epochs), results['trainLoss'], color='k', lw=1)
     ax[0].set_xlabel('Epoch')
     ax[0].set_ylabel('Loss')
@@ -219,11 +299,11 @@ def plotResults(results, args):
     ax[1].set_ylim(yMin, yMax)
     ax[1].set_title('Testing Loss')
 
-    ax[2].scatter(results['testSeqLength'], results['testLoss'], color='b', alpha=0.5)
-    ax[2].set_xlabel('Sequence Length')
-    ax[2].set_ylabel('Testing Loss')
-    # ax[2].set_ylim(yMin, yMax)
-    ax[2].set_title('Test Loss vs. Sequence Length')
+    # ax[2].scatter(results['testSeqLength'], results['testLoss'], color='b', alpha=0.5)
+    # ax[2].set_xlabel('Sequence Length')
+    # ax[2].set_ylabel('Testing Loss')
+    # # ax[2].set_ylim(yMin, yMax)
+    # ax[2].set_title('Test Loss vs. Sequence Length')
 
     if not(args.nosave):
         plt.savefig(str(figsPath/getFileName()))
