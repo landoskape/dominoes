88f323dff10cd3b699924921af94f5671ff1306a
landoskape <andrewtylerlandau@gmail.com>
2024-05-09 16:34:05 +0100

    add required method

--- dominoes/networks/__init__.py
+++ dominoes/networks/__init__.py
@@ -1,6 +1,6 @@
 import torch
 from ..utils import named_transpose
-from .pointer_networks import get_pointer_network
+from .pointer_networks import get_pointer_network, get_pointer_kwargs
 from .pointer_layers import get_pointer_methods
 
 
