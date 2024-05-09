f388073a001a5965f03149b9a54ab940cbb02eb0
landoskape <andrewtylerlandau@gmail.com>
2024-05-09 12:19:16 +0100

    add critical methods to init, and some support

--- dominoes/networks/__init__.py
+++ dominoes/networks/__init__.py
@@ -1,1 +1,9 @@
-from .pointers import get_pointer_methods, PointerNetwork
+from .pointer_networks import get_pointer_network
+from .pointer_layers import get_pointer_methods
+
+
+def _check_kwargs(method_name, kwargs, required_kwargs):
+    """method for checking kwargs for required arguments and returning useful errors"""
+    for key in required_kwargs:
+        if key not in kwargs:
+            raise ValueError(f"required kwarg {key} not found in kwargs ({method_name} requires {required_kwargs})")
