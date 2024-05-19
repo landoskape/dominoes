ad12a1aeac61099854d71c9853f9cbd543698f3b
landoskape <andrewtylerlandau@gmail.com>
2024-05-19 20:57:04 +0100

    prep testing boilerplate

--- dominoes/train.py
+++ dominoes/train.py
@@ -1,10 +1,11 @@
 from tqdm import tqdm
 import torch
-from .utils import named_transpose
 from .networks.net_utils import forward_batch
 from .networks.baseline import make_baseline_nets, check_baseline_updates
+from .utils import train_nets, test_nets
 
 
+@train_nets
 def train(nets, optimizers, dataset, **parameters):
     """a generic training function for pointer networks"""
     num_nets = len(nets)
@@ -153,3 +154,91 @@ def train(nets, optimizers, dataset, **parameters):
     )
 
     return results
+
+
+# @torch.no_grad()
+# @test_nets
+# def test(nets, optimizers, dataset, **parameters):
+#     """a generic boiler plate function for testing and evaluating pointer networks"""
+#     num_nets = len(nets)
+#     assert num_nets == len(optimizers), "Number of networks and optimizers must match"
+
+#     # get some key training parameters
+#     epochs = parameters.get("epochs")
+#     verbose = parameters.get("verbose", False)
+#     max_possible_output = parameters.get("max_possible_output")  # this is the maximum number of outputs ever
+#     temperature = parameters.get("temperature", 1.0)
+#     thompson = parameters.get("thompson", False)
+
+#     # process the learning_mode and save conditions
+#     get_loss = parameters.get("save_loss", False)
+#     get_reward = parameters.get("save_reward", False)
+
+#     # create some variables for storing data related to supervised loss
+#     if get_loss:
+#         test_loss = torch.zeros(epochs, num_nets, device="cpu")
+
+#     # create some variables for storing data related to rewards
+#     if get_reward:
+#         test_reward = torch.zeros(epochs, num_nets, device="cpu")
+#         test_reward_by_pos = torch.zeros(epochs, max_possible_output, num_nets, device="cpu")
+#         confidence = torch.zeros(epochs, max_possible_output, num_nets, device="cpu")
+
+#     # create dataset-specified variables for storing data
+#     dataset_variables = dataset.create_training_variables(num_nets, **parameters)
+
+#     # epoch loop
+#     epoch_loop = tqdm(range(epochs)) if verbose else range(epochs)
+#     for epoch in epoch_loop:
+#         # generate a batch
+#         batch = dataset.generate_batch(**parameters)
+
+#         scores, choices = forward_batch(nets, batch, max_possible_output, temperature, thompson)
+
+#         # get loss
+#         if get_loss:
+#             loss = dataset.measure_loss(scores, batch["target"], check_divergence=True)
+
+#         # get reward
+#         if get_reward:
+#             rewards = [dataset.reward_function(choice, batch) for choice in choices]
+
+#         # update networks
+#         for opt in optimizers:
+#             opt.step()
+
+#         # save training data
+#         if get_loss:
+#             for i in range(num_nets):
+#                 test_loss[epoch, i] = loss[i].detach().cpu()
+
+#         if get_reward:
+#             pretemp_scores = dataset.get_pretemp_scores(scores, choices, temperature)
+#             for i in range(num_nets):
+#                 test_reward[epoch, i] = torch.mean(torch.sum(rewards[i], dim=1)).detach().cpu()
+#                 test_reward_by_pos[epoch, :, i] = torch.mean(rewards[i], dim=0).detach().cpu()
+#                 confidence[epoch, :, i] = torch.mean(pretemp_scores[i], dim=0).detach().cpu()
+
+#         # save dataset-specific variables
+#         epoch_state = dict(
+#             epoch=epoch,
+#             batch=batch,
+#             scores=scores,
+#             choices=choices,
+#             loss=loss if get_loss else None,
+#             rewards=rewards if get_reward else None,
+#             gamma_transform=None,
+#             temperature=temperature,
+#         )
+#         dataset.save_training_variables(dataset_variables, epoch_state, **parameters)
+
+#     # return training data
+#     results = dict(
+#         test_loss=test_loss if get_loss else None,
+#         test_reward=test_reward if get_reward else None,
+#         test_reward_by_pos=test_reward_by_pos if get_reward else None,
+#         confidence=confidence if get_reward else None,
+#         dataset_variables=dataset_variables,
+#     )
+
+#     return results
